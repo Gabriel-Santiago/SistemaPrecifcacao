5d2bd9fc3104efc8a28b54581c3ed5820fb72ad4
Gabriel <gabrielsrmj@alu.ufc.br>
2023-02-27 08:26:19 -0300

    Base do projeto

---
+++ src/main/java/br/com/SistemaPrecificacao/SistemaPrecificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.SistemaPrecificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPrecificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPrecificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/SistemaPrecificacao/SistemaPrecificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SistemaPrecificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPrecificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
